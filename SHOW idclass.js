848d48ff24265122553203a290a6d5ff1f801333
budinusi74 <151961031+budinusi74@users.noreply.github.com>
2024-04-08 22:11:36 +0800

    Add files via upload

---
+++ SHOW idclass.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+$("#AA0").click(function(){
+$(".top").show();
+$(".hide").hide();
+});  
+$("#AA1").click(function(){
+$("div, #1").show();
+$("#0, #2").hide();
+});
+$("#AA2").click(function(){
+$("div, #2").show();
+$("#0, #1").hide();
+});
+$("#show").click(function(){
+$("div, #0, #1, #2").show();
+});
+});
