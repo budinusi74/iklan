81aeb44225dc5883c9e297bea1e8873589090fc5
budinusi74 <151961031+budinusi74@users.noreply.github.com>
2024-04-10 20:29:55 +0800

    Update SHOW idclass.js

--- SHOW idclass.js
+++ SHOW idclass.js
@@ -6,13 +6,13 @@ $(".hide").hide();
 });  
 $("#AA1").click(function(){
 $("div, #1").show();
-$("#0, #2").hide();
+$("#0, #2 #3").hide();
 });
 $("#AA2").click(function(){
-$("div, #2").show();
+$("div, #2, #3").show();
 $("#0, #1").hide();
 });
 $("#show").click(function(){
-$("div, #0, #1, #2").show();
+$("div, #0, #1, #2, #3").show();
 });
 });
