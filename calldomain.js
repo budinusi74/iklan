10e3385c9f71439cb3c91bcda8b98a2228863fdb
budinusi74 <151961031+budinusi74@users.noreply.github.com>
2024-04-12 06:24:53 +0800

    Update calldomain.js

--- calldomain.js
+++ calldomain.js
@@ -160,7 +160,7 @@ function myFunction() {
 }
 
 // Get the modal
-var modalbahaya1 = document.getElementById('pdewatogel, pppkoki, pauto177, pbetcoinasia, pantiktoto');
+var modalbahaya1 = document.getElementById('pdewatogel, p');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
