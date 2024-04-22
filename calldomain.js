427a4a48611d02af8ae46f76ca89575347c2afd7
budinusi74 <151961031+budinusi74@users.noreply.github.com>
2024-04-22 23:26:48 +0800

    Update calldomain.js

--- calldomain.js
+++ calldomain.js
@@ -1,4 +1,5 @@
 function xtogeltoto() {window.open("https://noblockss.blogspot.com/");}
+function xcor77() {window.open("https://cor77.co/");}
 function xvit88() {window.open("https://vit88f.com/");}
 function xadipatislot() {window.open("https://adipatislot-vip.com/");}
 function xlotuspelangi() {window.open("https://pelangihujan.online/daftar?ref=rakuyaba");}
