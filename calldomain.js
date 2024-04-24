7cc2bce88986551bbfcdc4459da1a2d214154646
budinusi74 <151961031+budinusi74@users.noreply.github.com>
2024-04-25 07:25:57 +0800

    Update calldomain.js

--- calldomain.js
+++ calldomain.js
@@ -85,7 +85,7 @@ function xbaron4d() {window.open("https://baron5.xyz/web/register?ref=rakuyaba")
 function xliga365new() {window.open("https://newliga365e.xyz/?action=register&rc=N8SY7QO738&ci=11");}
 function xslotoppo88() {window.open("https://obattidurlelap.com/?ref=rakuyaba");}
 function xjadijp() {window.open("https://jadijp2.com/");}
-function xwisdom77() {window.open("https://wisdom77go.beauty/?ref=rakuyaba");}
+function xwisdom77() {window.open("https://wisdom77go.hair/?ref=rakuyaba");}
 function xlembagatoto() {window.open("http://lembagatoto18.com/register?referral_code=rakuyaba");}
 function xw77() {window.open("https://w-jamingacor.org/");}
 function xunoplay() {window.open("https://www.unoplay7s.com/register?ref=E48A47B5");}
