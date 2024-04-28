3d67d4b9d0c2603fd5d0c5a47fa33443b06cd4f0
budinusi74 <151961031+budinusi74@users.noreply.github.com>
2024-04-28 18:33:12 +0800

    Add files via upload

---
+++ menuatas.js
@@ -0,0 +1,7 @@
+function opentutorialvideo() {
+  document.getElementById("videotutorialshow").style.height = "100%";
+}
+
+function closetutorialvideo() {
+  document.getElementById("videotutorialshow").style.height = "0%";
+}
---
+++ textjalan.js
@@ -0,0 +1,7 @@
+function opentutorial() {
+  document.getElementById("tutorialshow").style.height = "100%";
+}
+
+function closetutorial() {
+  document.getElementById("tutorialshow").style.height = "0%";
+}
