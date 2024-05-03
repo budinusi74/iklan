bca7b65f6e3a886906bc7c521db6849902b75899
budinusi74 <151961031+budinusi74@users.noreply.github.com>
2024-05-03 11:58:53 +0800

    Update calldomain.js

--- calldomain.js
+++ calldomain.js
@@ -26,7 +26,7 @@ function xtektok77() {window.open("https://tektok77thai.com/register?ref=cvdwOsa
 function xdauntogel() {window.open("https://daunkeren.com/link.php?member=rakuyaba");}
 function xindogenting() {window.open("https://www.indogenting0324e.com/");}
 function xkenzo188() {window.open("https://waktukenzo.shop/register?ref=A3MMMAF4PO7");}
-function xpmg88() {window.open("https://www.pmg88ok.site/register?ref=u2y78Bp");}
+function xpmg88() {window.open("https://pmg88daftar.site/register?ref=u2y78Bp");}
 function xagen288() {window.open("https://agen288oke.com/");}
 function xnyaitogel() {window.open("http://nyaimania.com/register?referral_code=rakuyaba");}
 function xpintutogel() {window.open("http://mainpintutogel.com/register?referral_code=rakuyaba");}
