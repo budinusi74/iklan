149e2738ab6ea49346b0c34d3ab6969f50d7a8b1
budinusi74 <151961031+budinusi74@users.noreply.github.com>
2024-05-05 14:53:38 +0800

    Update calldomain.js

--- calldomain.js
+++ calldomain.js
@@ -79,7 +79,7 @@ function xmpo333() {window.open("https://mpo333j.com/register/YGN4232D");}
 function xnaga007() {window.open("https://anggurpaten.site/register?r=groty8");}
 function xjitu77() {window.open("https://vipjitu77.click/");}
 function xeraplay88() {window.open("https://era99play.org/");}
-function xmpo222() {window.open("https://mpo222jitu.com/register/WNR6893G");}
+function xmpo222() {window.open("https://mpo222wir.com/register/WNR6893G");}
 function xmpo111() {window.open("https://mpo111sp.com/");}
 function xmpo999() {window.open("https://mpo999.com/register/Y654G7R0");}
 function xmpo777() {window.open("https://mpo777timur.com/register/YGNBJ4MR");}
