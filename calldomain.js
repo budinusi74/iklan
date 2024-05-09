653f05333c53bf19db06e731f4ee621d246c3f4f
budinusi74 <151961031+budinusi74@users.noreply.github.com>
2024-05-09 11:44:12 +0800

    Update calldomain.js

--- calldomain.js
+++ calldomain.js
@@ -112,7 +112,7 @@ function xajr88() {window.open("https://ajr88jos.site/");}
 function xhoki178() {window.open("https://hk178.club/");}
 function xdewagacor77() {window.open("https://dewagacor77.work/");}
 function xkudapoker() {window.open("https://kkuudapokerr.co/");}
-function xvipbet88() {window.open("https://vip88go.live/");}
+function xvipbet88() {window.open("https://vipbet88gm.pro/");}
 function xpesiar88() {window.open("https://pesiar88omg.ink/");}
 function xpokerbo() {window.open("https://pokerbov.lol/");}
 function xsinggah4d() {window.open("https://singgah4d-gacor.com/?ref=rakuyaba");}
